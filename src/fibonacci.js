5b82287b0d844ab02fafb77706d983211f84f582
cesar-js-23 <80943995+cesar-js-23@users.noreply.github.com>
2022-09-05 11:18:28 -0500

    Update fibonacci.js

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,26 @@
 const fibonacci = (n) => {
   // your code here
+  let num1 = 1;
+  let num2 = 1;
+  let aux = 0;
+  let concatArray = [1, 1];
+  let newArray = [];
+  if (n === 1) {
+    newArray.push(num1);
+    return newArray;
+  }
+  if (n === 2) {
+    newArray.push(num1);
+    newArray.push(num2);
+    return newArray;
+  }
+  for (let i = 2; i < n; i++) {
+    aux = num1 + num2;
+    num1 = num2;
+    num2 = aux;
+    newArray.push(aux);
+  }
+  return concatArray.concat(newArray);
 }
 
 module.exports = fibonacci;
